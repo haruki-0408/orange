e5ac955e3376c7a7a0dee9b88cec8622cc1b4fe5
a3a <94882349+haruki-0408@users.noreply.github.com>
2025-01-16 12:53:27 +0900

    Update stateNameLogGroupNameMapping.ts

--- src/features/workflow/const/stateNameLogGroupNameMapping.ts
+++ src/features/workflow/const/stateNameLogGroupNameMapping.ts
@@ -1,13 +1,25 @@
-export const stateNameLogGroupNameMapping =  {
-  "format-lambda": "/aws/lambda/melon_dev_get_fake_thesis_title_category_format",
-  "prompt-lambda": "/aws/lambda/melon_dev_generate_prompt_parameters",
-  "ai-request-lambda": "/aws/lambda/melon_dev_request_generative_ai_model_api",
-  "validation-lambda": "/aws/lambda/melon_dev_fake_thesis_data_validation",
-  "data-fix-lambda": "/aws/lambda/melon_dev_fix_fake_thesis_data",
-  "callback-success-lambda": "/aws/lambda/melon_dev_send_workflow_callback",
-  "formula-gen-lambda": "/aws/lambda/melon_dev_generate_fake_formula",
-  "table-gen-lambda": "/aws/lambda/melon_dev_generate_fake_table",
-  "graph-gen-lambda": "/aws/lambda/melon_dev_generate_fake_graph",
-  "pdf-format-lambda": "/aws/lambda/melon_dev_convert_to_pdf"
-}
-  
+export const stateNameLogGroupNameMapping =  process.env.NODE_ENV === 'production'
+  ? {
+      "format-lambda": "/aws/lambda/melon_prd_get_fake_thesis_title_category_format",
+      "prompt-lambda": "/aws/lambda/melon_prd_generate_prompt_parameters",
+      "ai-request-lambda": "/aws/lambda/melon_prd_request_generative_ai_model_api",
+      "validation-lambda": "/aws/lambda/melon_prd_fake_thesis_data_validation",
+      "data-fix-lambda": "/aws/lambda/melon_prd_fix_fake_thesis_data",
+      "callback-success-lambda": "/aws/lambda/melon_prd_send_workflow_callback",
+      "formula-gen-lambda": "/aws/lambda/melon_prd_generate_fake_formula",
+      "table-gen-lambda": "/aws/lambda/melon_prd_generate_fake_table",
+      "graph-gen-lambda": "/aws/lambda/melon_prd_generate_fake_graph",
+      "pdf-format-lambda": "/aws/lambda/melon_prd_convert_to_pdf"
+    }
+  : {
+      "format-lambda": "/aws/lambda/melon_dev_get_fake_thesis_title_category_format",
+      "prompt-lambda": "/aws/lambda/melon_dev_generate_prompt_parameters",
+      "ai-request-lambda": "/aws/lambda/melon_dev_request_generative_ai_model_api",
+      "validation-lambda": "/aws/lambda/melon_dev_fake_thesis_data_validation",
+      "data-fix-lambda": "/aws/lambda/melon_dev_fix_fake_thesis_data",
+      "callback-success-lambda": "/aws/lambda/melon_dev_send_workflow_callback",
+      "formula-gen-lambda": "/aws/lambda/melon_dev_generate_fake_formula",
+      "table-gen-lambda": "/aws/lambda/melon_dev_generate_fake_table",
+      "graph-gen-lambda": "/aws/lambda/melon_dev_generate_fake_graph",
+      "pdf-format-lambda": "/aws/lambda/melon_dev_convert_to_pdf"
+    };
